dd5612e8a901da8ede08779ffa2b6963acd88937
Drumnicorn <robin@seerig.de>
2023-11-08 09:04:45 +0100

    changed back i guess

--- src/_codux/boards/landing-page.board.tsx
+++ src/_codux/boards/landing-page.board.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { createBoard } from '@wixc3/react-board';
+import Page from '../../../app/page';
+import Layout from '../../../app/layout';
 
 export default createBoard({
-    name: 'Home',
+    name: 'Landing Page',
     Board: () => <div>
-        <nav><a href="/home">Home</a> | <a href="/projects">Projects</a> | <a href="/about">About</a> | <a href="/contact">Contact Us</a></nav>
+        <Page />
+        <Layout />
     </div>,
     isSnippet: true,
 });
