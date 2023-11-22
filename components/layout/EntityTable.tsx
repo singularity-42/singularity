53e9f97e82b4c2ead9d70f4dab2f62d995e4a084
Drumnicorn <robin@seerig.de>
2023-11-22 13:29:55 +0100

    add order for table

--- components/layout/EntityTable.tsx
+++ components/layout/EntityTable.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React, { useEffect, useState } from 'react';
 import styles from '../../styles/EntityTable.module.scss';
 import EntityFilter from './EntityFilter';
@@ -7,37 +5,58 @@ import EntityList from './EntityList';
 import useEntityData from '@/hooks/useEntityData';
 import Loading from '../util/view/Loading';
 
-interface EntityTableProps {
-    type: string;
+export enum OrderType {
+   Alphabetical = "Alphabetical",
+   Random = "Random",
+   CounterAlphabetical = "CounterAlphabetical"
 }
 
-const EntityTable: React.FC<EntityTableProps> = ({ type }) => {
-    const [filter, setFilter] = useState<string[]>([]);
-    const entityData = useEntityData(type, filter);
+interface EntityTableProps {
+   type: string;
+   orderType?: OrderType;
+}
 
-    const [currentVisibleTags, setCurrentVisibleTags] = useState<string[]>([]);
+const EntityTable: React.FC<EntityTableProps> = ({ type, orderType = OrderType.Alphabetical }) => {
+   const [filter, setFilter] = useState<string[]>([]);
+   const entityData = useEntityData(type, filter);
 
-    useEffect(() => {
-        let tags = entityData?.map(entity => entity.metadata.tags);
-        // flatten tags
-        tags = tags?.reduce((acc, val) => acc.concat(val), []);
-        setCurrentVisibleTags(tags ?? []);
-    }, [entityData]);
+   const [currentVisibleTags, setCurrentVisibleTags] = useState<string[]>([]);
 
-    if (!entityData) {
-        return <Loading />
-    }
+   useEffect(() => {
+       let tags = entityData?.map(entity => entity.metadata.tags);
+       // flatten tags
+       tags = tags?.reduce((acc, val) => acc.concat(val), []);
+       setCurrentVisibleTags(tags ?? []);
+   }, [entityData]);
 
-    return <div className={styles.entityTable}>
-        <EntityFilter filter={filter} currentVisibleTags={currentVisibleTags} onChange={setFilter} />
-        <EntityList entityData={entityData} onTagClick={(tag: string) => {
-            setFilter([...filter, tag]);
-        }} />
-        {/*
-        <EntityPagination /> // for pagination of entitys (by default 10 entitys per page)
-        <EntityCreate /> // for creating a new entity
-        */}
-    </div>;
+   useEffect(() => {
+       switch(orderType) {
+           case OrderType.Alphabetical:
+               entityData?.sort((a, b) => a.metadata.title.localeCompare(b.metadata.title));
+               break;
+           case OrderType.Random:
+               entityData?.sort(() => Math.random() - 0.5);
+               break;
+           case OrderType.CounterAlphabetical:
+               entityData?.sort((a, b) => b.metadata.title.localeCompare(a.metadata.title));
+               break;
+       }
+   }, [orderType, entityData]);
+
+   if (!entityData) {
+       return <Loading />
+   }
+
+   return <div className={styles.entityTable}>
+       <EntityFilter filter={filter} currentVisibleTags={currentVisibleTags} onChange={setFilter} />
+       <EntityList entityData={entityData} onTagClick={(tag: string) => {
+           setFilter([...filter, tag]);
+       }} />
+       {/*
+       <EntityPagination /> // for pagination of entitys (by default 10 entitys per page)
+       <EntityCreate /> // for creating a new entity
+       */}
+   </div>;
 };
 
 export default EntityTable;
