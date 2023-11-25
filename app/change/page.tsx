f6c7f5e8daa95c287f91b2ceac0b0719ce0a2798
Drumnicorn <robin@seerig.de>
2023-11-25 23:18:17 +0100

    added changes page and examples

---
+++ app/change/page.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import TableContent from '@/components/layout/Table';
+
+const CreativesPage: React.FC = () => {
+  return (
+      <TableContent type='change' />
+  );
+};
+
+export default CreativesPage;
