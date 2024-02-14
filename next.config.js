54c93391a643307d109d36c08e3497bf8bf71351
Drumnicorn <robin@seerig.de>
2024-02-14 08:53:51 +0100

    build fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['cdninstagram.com', 'singularity.2n40.eu'],
-    },  
 }
-
-if (process.env.NODE_ENV !== 'production') nextConfig.images.domains.push('localhost')
 
 module.exports = nextConfig
